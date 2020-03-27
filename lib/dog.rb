ef59b15f6506987b7676f543575e1bfceedfcf8d
Micah Banschick <micha.ban@gmail.com>
2020-03-27 19:29:07 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,73 @@
+class Dog 
+  
+  attr_accessor :name, :breed, :id
+
+  @@all = []
+  
+  def initialize(params)
+    @name = params[:name]
+    @breed = params[:breed]
+    @@all << self
+  end
+  
+  def self.all 
+    @@all.dup.freeze  
+  end 
+  
+  def self.create_table 
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+    SQL
+    DB[:conn].execute(sql)
+  end 
+  
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs" 
+    DB[:conn].execute(sql)
+  end 
+  
+  def save
+    sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
+    @id = DB[:conn].execute(sql, self.name, self.breed)[0]
+    self
+  end 
+  
+  def self.create(params)
+    self.new(params).save
+  end 
+  
+  def self.new_from_db(row) 
+  #binding.pry
+    params = {:name => row[1], :breed => row[2]}
+    new_dog = self.new(params)
+    new_dog.id = row[0]
+    new_dog
+  end 
+  
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    row = DB[:conn].execute(sql, id)[0]
+    self.new_from_db(row)
+  end 
+  
+  def self.find_or_create_by(name:, breed:)
+    #binding.pry 
+    sql = "SELECT * FROM dogs WHERE name = ? AND breed = ?"
+    row = DB[:conn].execute(sql, name, breed)[0]
+    params = {:name => name, :breed => breed}
+    row.any?{|data| data == nil} ? self.find_by_id(row[0]) : self.create(params)
+  end 
+  
+  def self.find_by_name
+  end 
+  
+  def update 
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)[0]
+  end 
+  
+end 

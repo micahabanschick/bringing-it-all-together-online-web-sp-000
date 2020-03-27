66eb9bfba7326c6f5afab4dd7335031bbc463831
Micah Banschick <micha.ban@gmail.com>
2020-03-27 19:37:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,7 +33,7 @@ class Dog
   def save
     insert_sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
     select_sql = "SELECT id FROM dogs WHERE name = ? AND breed = ?"
-    #@id = DB[:conn].execute(insert_sql, self.name, self.breed)[0]
+    @id = DB[:conn].execute(insert_sql, self.name, self.breed)[0]
     @id = DB[:conn].execute(select_sql, self.name, self.breed)[0]
     self
   end 
